04702c7ecda67129d3c01407cb4753f0b3209126
ddsomav8 <yuliangchenzh@163.com>
2018-09-07 13:12:00 +0800

    1

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,6 +3,8 @@
 set -e
 npm run docs:build
 
+cd docs/.vuepress/dist
+
 git init
 git add -A
 git commit -m 'deploy'
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,26 @@
 module.exports = {
+  base: '/vuepress-learn/',
   title: 'hello vuepress',
-  description: 'just playing around'
+  description: 'just playing around',
+
+  themeConfig: {
+    repo: 'ddsomav8/vuepress-learn',
+    nav: [
+      {
+        text: '正文',
+        link: '/art/'
+      }
+    ],
+    sidebar: {
+      '/art/': [
+        {
+          title: '我是标题',
+          children: [
+            '',
+            '1'
+          ]
+        }
+      ]
+    }
+  }
 }
